c6bd38fb864c1f62a346a2c735d83a506b1a2ef0
walidmohamedfarag <walid492003@gmail.com>
2025-05-12 01:22:40 +0300

    the file contain array data structure

---
+++ Array_Datastructur.cs
@@ -0,0 +1,95 @@
+using System;
+public class MyArray
+{
+    int Size;
+    int Length;
+    int[] arrayitems;
+    public MyArray(int size)
+    {
+        Size=size;
+        arrayitems=new int[size];
+        Length=0;
+    }
+    public int GetSize { get {return Size; } }
+    public int GetLength { get {return Length; } }
+
+    public void Fill(int length)
+    {
+        if(length>Size)
+        {
+            Console.WriteLine("the length is greater than size");
+            return;
+        }
+        else
+        {
+            for(int index=0; index<length; index++)
+            {
+                Console.Write($"enter the item number ({index+1}) : ");
+                int enteritem=int.Parse(Console.ReadLine());
+                arrayitems[index]=enteritem;
+                Length++;
+                
+            }
+        }
+    }
+    public void Dispaly()
+    {
+        for(int index=0; index<Length; index++)
+        {
+        Console.WriteLine($"the item number ({index+1}) : {arrayitems[index]}");
+        }
+    }
+    public void Add(int item)
+    {
+        if(item < 0)
+        throw new Exception("item must be equl or graeter than 0");
+        arrayitems[Length]=item;
+        Length++;
+
+    }
+    public void Append(int index,int item)
+    {
+        if(index>Size)
+        throw new Exception("index out of the range");
+
+        for(int iteration = Length-1; iteration >= index; iteration--)
+            arrayitems[iteration+1]=arrayitems[iteration];
+        arrayitems[index]=item;
+        Length++;
+        
+    }
+    public int Delete(int index)
+    {
+        if(index > Size)
+        throw new Exception("index out of the range");
+        int itemdelete = arrayitems[index];
+        for(int iteration = index; iteration < Length; iteration++)
+           arrayitems[iteration]=arrayitems[iteration + 1];
+        Length--; 
+        return itemdelete; 
+        
+    }
+    public void EnLarge(int newSize)
+    {
+        int[] _oldArray=new int[newSize];
+        Size=newSize;
+        for(int index = 0; index < Length; index++)
+        _oldArray[index]=arrayitems[index];
+    }
+    public void Marge(MyArray arraytomarge)
+    {
+        int newsize = Size + arraytomarge.Size;
+        Size = newsize;
+        int[] olditemarray=arrayitems;
+        arrayitems = new int[newsize];
+        int index;
+        for(index = 0; index < Length; index++)
+            arrayitems[index]=olditemarray[index];
+        for(int _index = 0; _index < arraytomarge.Length; _index++)
+        {
+            arrayitems[index++]=arraytomarge.arrayitems[_index];
+            Length++;
+        }
+
+    }
+}
