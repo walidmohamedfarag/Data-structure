a386de21bc347f4787fc1a637d5a3e46af527f43
walidmohamedfarag <walid492003@gmail.com>
2025-05-14 05:44:45 +0300

    the linked lis file is added

---
+++ My_Linked.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Linked_List
+{
+    internal class My_Linked
+    {
+        Node First;
+        public My_Linked()
+        {
+            First = null;
+        }
+        bool IsEmpty()
+        {
+            return First == null;
+        }
+        public int GetCounter { get { return Counter(); } }
+        int Counter()
+        {
+            Node tempPointer = First;
+            int counter = 0;
+            while (tempPointer != null)
+            {
+                counter++;
+                tempPointer = tempPointer.Next;
+            }
+            return counter;
+        }
+        public void Add(int item)
+        {
+            Node newnode = new Node(item);
+            if(IsEmpty())
+                First = newnode;
+            else
+            {
+                newnode.Next = First;
+                First = newnode;
+            }
+        }
+        public void Print()
+        {
+            Node tempPointer = First;
+            while(tempPointer != null)
+            {
+                Console.WriteLine(tempPointer.Data);
+                tempPointer = tempPointer.Next;
+            }
+        }
+        public void AddBefore(int item, int newitem)
+        {
+            Node temppointer = First;
+            while(temppointer != null && temppointer.Next.Data != item)
+            {
+                temppointer = temppointer.Next;
+            }
+            Node newnode = new Node(newitem);
+            newnode.Next = temppointer.Next;
+            temppointer.Next = newnode;
+        }
+        public void AddLast(int item)
+        {
+            Node temppointer = First;
+            while(temppointer.Next != null)
+            {
+                temppointer = temppointer.Next;
+            }
+            Node newnode = new Node(item);
+            newnode.Next = null;
+            temppointer.Next = newnode;
+        }
+
+    }
+}
