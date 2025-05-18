22d4357547aae28546b00293f89ff38dd8278c60
walidmohamedfarag <walid492003@gmail.com>
2025-05-18 11:01:45 +0300

    the new file (stack) is added

---
+++ MySatack.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Stack_Datastructure
+{
+    internal class MySatack
+    {
+        Node First;
+        public MySatack()
+        {
+            First = null;
+        }
+        bool IsEbpty()
+        {
+            return First == null;
+        }
+        public int GetCount { get { return count(); } }
+        public void Push(int item)
+        {
+            Node newnode = new Node(item);
+            if(IsEbpty())
+            {
+                First = newnode;
+                newnode.Next = null;
+            }
+            else
+            {
+                newnode.Next = First;
+                First = newnode;
+            }
+        }
+        public int Pop()
+        {
+            if(IsEbpty())
+                throw new Exception("Stack is empty");
+            Node tempointer = First;
+            int popitem = tempointer.Data;
+            tempointer = tempointer.Next;
+            First = tempointer;
+            return popitem;
+        }
+        public void PrintAllItem()
+        {
+            Node tempointer = First;
+            while (tempointer != null)
+            {
+                Console.WriteLine(tempointer.Data);
+                tempointer = tempointer.Next;
+            }
+        }
+        public int Peek()
+        {
+            if (IsEbpty())
+                throw new Exception("Stack is empty");
+            return First.Data;
+        }
+        private int count()
+        {
+            Node tempointer = First;
+            int count = 0;
+            while (tempointer != null)
+            {
+                tempointer = tempointer.Next;
+                count++;
+            }
+            return count;
+        }
+        public bool Search(int item)
+        {
+            Node tempointer = First;
+            while(tempointer != null)
+            {
+                if(tempointer.Data == item)
+                    return true;
+                tempointer = tempointer.Next;
+            }
+            return false;
+        }
+    }
+}
